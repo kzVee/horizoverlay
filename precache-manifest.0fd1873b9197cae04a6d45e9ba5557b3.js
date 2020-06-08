f0cc9f14046c7f548151b1c9aca00d6ad1d81ffd
kzVee <kzVee@users.noreply.github.com>
2020-06-08 00:25:42 -0300

    Updates

--- precache-manifest.0fd1873b9197cae04a6d45e9ba5557b3.js
+++ precache-manifest.0fd1873b9197cae04a6d45e9ba5557b3.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4f2f307e82c2549f693bebfbc97570cf",
+    "revision": "ad02f7f5bf1e38d4bd55af34a63248f8",
     "url": "/horizoverlay/index.html"
   },
   {
-    "revision": "1f77317754886c619e1d",
-    "url": "/horizoverlay/static/css/main.4a5cd103.chunk.css"
+    "revision": "94a5458ea110910e0ab5",
+    "url": "/horizoverlay/static/css/main.769bc40d.chunk.css"
   },
   {
     "revision": "7a2f89d10b511b9c0d33",
     "url": "/horizoverlay/static/js/2.b7868ab6.chunk.js"
   },
   {
-    "revision": "1f77317754886c619e1d",
-    "url": "/horizoverlay/static/js/main.b3223ba4.chunk.js"
+    "revision": "94a5458ea110910e0ab5",
+    "url": "/horizoverlay/static/js/main.f275a595.chunk.js"
   },
   {
     "revision": "f5cff2eb4effd23c272c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/horizoverlay/precache-manifest.6b1956371b0adeffb993848162adf9df.js"
+  "/horizoverlay/precache-manifest.0fd1873b9197cae04a6d45e9ba5557b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/horizoverlay/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

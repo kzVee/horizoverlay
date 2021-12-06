dc4d8e35ccd779ec801669078269e9ada3b56865
kzVee <darkkazeras@hotmail.com>
2021-12-06 04:44:57 -0300

    Updates

---
+++ precache-manifest.81338a060ccb72842110c5039a54b31d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30828408c6f715927afeafe9a28bb4fc",
+    "url": "/horizoverlay/index.html"
+  },
+  {
+    "revision": "ce68246df3163e65adc6",
+    "url": "/horizoverlay/static/css/main.e203ef0a.chunk.css"
+  },
+  {
+    "revision": "8888d3912746a078e4a0",
+    "url": "/horizoverlay/static/js/2.ce8b7888.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/horizoverlay/static/js/2.ce8b7888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce68246df3163e65adc6",
+    "url": "/horizoverlay/static/js/main.71529053.chunk.js"
+  },
+  {
+    "revision": "30f65308d7bdcba22167",
+    "url": "/horizoverlay/static/js/runtime-main.9a241b58.js"
+  },
+  {
+    "revision": "e1e188201661cad8e440f57279b85a23",
+    "url": "/horizoverlay/static/media/mog.e1e18820.png"
+  },
+  {
+    "revision": "65f664f02c5859261788294137730031",
+    "url": "/horizoverlay/static/media/rpr.65f664f0.png"
+  },
+  {
+    "revision": "745685d57c003483e11968290237d274",
+    "url": "/horizoverlay/static/media/sge.745685d5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/horizoverlay/precache-manifest.a7b0752f833f3f8baf31057bedd298bf.js"
+  "/horizoverlay/precache-manifest.81338a060ccb72842110c5039a54b31d.js"
 );
 
 self.addEventListener('message', (event) => {
